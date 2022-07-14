1260a0c7d4f888fa7f4175bff603d86b1544f182
Nhat <nhaatn1@gmail.com>
2022-07-14 00:58:29 -0600

    added super basic modal using classlist add / remove

---
+++ 07_modal/index.js
@@ -0,0 +1,11 @@
+const modal_btn = document.querySelector('#modal-btn')
+const modal_overlay = document.querySelector('.modal-overlay')
+const close_btn = document.querySelector('#close-btn')
+
+modal_btn.addEventListener('click', () => {
+    modal_overlay.classList.add('open-modal');
+})
+
+close_btn.addEventListener('click', () => {
+    modal_overlay.classList.remove('open-modal');
+})
